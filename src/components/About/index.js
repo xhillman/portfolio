f8962bf39339bd96e35aa6b8df1f9d100aa6cbf6
Xavier Hillman <xhillman13@gmail.com>
2022-12-20 00:06:29 -0500

    touch up about styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,13 @@ const About = () => {
     <div className='about' id='about'>
       <div className='about-container'>
         <div className='about-context'>
-          <Title order={2} variant='gradient' gradient={{ from: 'green', to: 'blue' }} className='about-title'>About Me</Title>
+          <Title
+            order={2}
+            variant='gradient'
+            gradient={{ from: 'green', to: 'blue' }}
+            className='about-title'>
+            About Me
+          </Title>
           <Text className='about-text'>
             Hello! My name's Xavier and I enjoy bringing things to life with code. I got my start in web development in 2020 when I learned Python through Stanford's Code in Place course. I fell in love with the process and started tinkering with HTML, CSS, and JavaScript. After a few months of self-teaching, I decided to take the plunge and enroll in a coding bootcamp. I graduated from Code Fellows in 2022 with a certificate in Advanced Software Development with JavaScript. Some of the technologies that I've been working with recently include:
           </Text>
