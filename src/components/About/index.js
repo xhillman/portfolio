d5f3cdd428648317b34d891ec8ad906b2d976b70
Xavier Hillman <xhillman13@gmail.com>
2023-02-01 13:51:55 -0500

    fix background gap

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -15,7 +15,7 @@ const About = () => {
             About Me
           </Title>
           <Text className='about-text'>
-            Hello! My name's Xavier and I enjoy bringing things to life with code. I got my start in web development in 2020 when I learned Python through Stanford's Code in Place course. I fell in love with the process and started tinkering with HTML, CSS, and JavaScript. After a few months of self-teaching, I decided to take the plunge and enroll in a coding bootcamp. I graduated from Code Fellows in 2022 with a certificate in Advanced Software Development with JavaScript. Some of the technologies that I've been working with recently include:
+            Hello! My name's Xavier and I enjoy bringing things to life with code. I got my start in web development in 2020 when I learned Python through Stanford's Code in Place course. I fell in love with the process and started tinkering with HTML, CSS, and JavaScript. After a few months of self-teaching, I decided to take the plunge and enroll in a coding bootcamp. I graduated from Code Fellows in 2022 with a certificate in Advanced Software Development with Full-Stack JavaScript. Some of the technologies that I've been working with recently include:
           </Text>
           <ul className='tech-list'>
             <li className='tech-list-item'>JavaScript</li>
